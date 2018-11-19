aaa6170f42ca99bf1ff2f1496d2541394fd25804
spzvtbg <spzvtbg@mail.de>
2018-11-19 13:20:39 +0100

    template folder for all solutions

--- 02 JS Advanced/02 Dom manipulations/14 Distance converter/distance.js
+++ 02 JS Advanced/02 Dom manipulations/14 Distance converter/distance.js
@@ -13,10 +13,10 @@ function attachEventsListeners () {
     document.getElementById('convert').addEventListener('click', convert);
 
     function convert () {
-        let inputMetric = document.getElementById('inputUnits').value;
-        let outputMetric = document.getElementById('outputUnits').value;
-        let distance = +document.getElementById('inputDistance').value;
-        let result = (distance * metrics[inputMetric]) / metrics[outputMetric];
+        const inputMetric = document.getElementById('inputUnits').value;
+        const outputMetric = document.getElementById('outputUnits').value;
+        const distance = +document.getElementById('inputDistance').value;
+        const result = (distance * metrics[inputMetric]) / metrics[outputMetric];
 
         document.getElementById('outputDistance').value = result;
     }
